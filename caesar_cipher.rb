2a467cf1365d743f4ad2d0318779e8a7f139764f
Sudarshan <bsudarshan@outlook.com>
2016-12-01 23:50:32 -0500

    finished algorithm... continuing testing

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -16,19 +16,31 @@ end
 #reference ASCII codes for numbers
 def caesar_cipher(input, cipher_key=1)
     to_cipher = input.split('')
-    to_cipher.each do |char, index|
+    to_cipher.each_with_index do |char, index|
         if is_alpha(char)
             if is_lower(char)
                 ckey = (char.ord - 'a'.ord + cipher_key) % 26 + 97
-                to_cipher[index] = (ckey).ch
+                to_cipher[index] = (ckey).chr
             end
             if is_upper(char)
                 ckey = (char.ord - 'A'.ord + cipher_key) % 26 + 65
-                to_cipher[index] = (ckey).ch
+                to_cipher[index] = (ckey).chr
             end
         end
     end
 end
-    
-puts "Test 1"
-caesar_cipher("ab", 1)
+
+#test vanilla
+puts caesar_cipher("ab").join("")
+
+#test key of 3
+puts caesar_cipher("ab",3).join("")
+
+#test spaces
+puts caesar_cipher("hello my name is bob the builder").join("")
+
+#test capital differentation
+puts caesar_cipher("JoKeR").join("")
+
+#test special chars
+puts caesar_cipher("!!!!hi!!!!").join("")
